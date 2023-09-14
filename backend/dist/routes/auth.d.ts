9fdb775cb0c07e4e45986fa48c1b4cda8b6b5354
melland55 <andrewmelland@gmail.com>
2023-09-14 14:22:17 -0700

    Remove dist directory from Git tracking

--- backend/dist/routes/auth.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-declare const authenticateToken: (req: Request, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-declare const authenticateUser: (req: Request, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-export { authenticateToken, authenticateUser };
